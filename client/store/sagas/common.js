23e7d748eee0b13443f4910484ce692603c3483f
Thanh Tu <tubackkhoa@gmail.com>
2016-11-21 13:38:27 +0700

    pass action to success callback of saga, to do next thing such as loadBooks when addBook succeeded

--- client/store/sagas/common.js
+++ client/store/sagas/common.js
@@ -22,7 +22,10 @@ export const createRequestSaga = ({request, key, start, stop, success, failure, 
   // we may not needs this if we use redux-saga, of course we can use both
   return function* (action) {    
     const requestKey = (typeof key === 'function') ? key(...action.args) : key
-
+    // for key, we render unique key using action.args
+    // but for actionCreator when callback, we should pass the whole action
+    // so on event such as success, we can use action.type or action.args to 
+    // do next, example: addBook => success : (data, {args:[token]}) => loadBooks(token) 
     if(start) for(let actionCreator of start){      
       yield put(actionCreator())
     }    
@@ -53,26 +56,26 @@ export const createRequestSaga = ({request, key, start, stop, success, failure, 
       } else if(cancelRet){
         // callback on success
         if(cancelled) for(let actionCreator of cancelled){          
-          yield put(actionCreator(cancelRet))
+          yield put(actionCreator(cancelRet, action))
         }
         // mark cancelled request
         yield put(markRequestCancelled(cancelRet, requestKey))
       } else {
         // callback on success
         if(success) for(let actionCreator of success){          
-          yield put(actionCreator(data))
+          yield put(actionCreator(data, action))
         }        
         yield put(markRequestSuccess(requestKey))
       }            
       
     } catch (reason) {
       if(failure) for(let actionCreator of failure){          
-        yield put(actionCreator(reason))
+        yield put(actionCreator(reason, action))
       }        
       yield put(markRequestFailed(reason, requestKey))
     } finally {
       if(stop) for(let actionCreator of stop){          
-        yield put(actionCreator(reason))
+        yield put(actionCreator(reason, action))
       } 
     }
   }
