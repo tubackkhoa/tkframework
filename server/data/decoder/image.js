1fa770808ea9862463504387402bc71734375583
Thanh Tu <tubackkhoa@gmail.com>
2016-11-24 13:40:12 +0700

    fix for image decode

--- server/data/decoder/image.js
+++ server/data/decoder/image.js
@@ -2,7 +2,7 @@ export const decodeBase64Image = (dataString) => {
   const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
   const response = {}
 
-  if (matches.length === 3) {
+  if (matches && matches.length === 3) {
     // from base64, image by default is png
     response.mimetype = matches[1]
     response.buffer = new Buffer(matches[2], 'base64')
