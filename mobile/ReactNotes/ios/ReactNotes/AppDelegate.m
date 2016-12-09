8d1ad0da24448b24450c5a72c88d28051fc7e3c7
Thanh Tu <tubackkhoa@gmail.com>
2016-12-09 10:58:03 +0700

    debug and release for react-native

--- mobile/ReactNotes/ios/ReactNotes/AppDelegate.m
+++ mobile/ReactNotes/ios/ReactNotes/AppDelegate.m
@@ -18,8 +18,12 @@
 {
   NSURL *jsCodeLocation;
 
+#ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ReactNotes"
                                                initialProperties:nil
--- mobile/ReactNotes/store/constants/api.js
+++ mobile/ReactNotes/store/constants/api.js
@@ -1,4 +1,2 @@
-export const API_BASE = 'http://localhost:3333'
-// export const API_BASE = 'http://react.agiletech.vn'
-
+export const API_BASE = __DEV__ ? 'http://localhost:3333' : 'http://react.agiletech.vn'
 export const API_TIMEOUT = 10000
